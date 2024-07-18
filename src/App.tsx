909f5716a033ebcad517eb8f400f6bb6cb11b209
吉村直人 <ahowateto123@gmail.com>
2024-07-18 21:39:51 +0900

    削除機能

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,21 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>
+              {todo.content}
+          </li>
         ))}
       </ul>
       <div>
